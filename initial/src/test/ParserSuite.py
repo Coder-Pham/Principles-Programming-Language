778c16bd6e7647548a7977ea8a5c067f31b3210f
Phạm Trọng Nhân <tendaigom@gmail.com>
2019-09-19 00:05:48 +0700

    [Add] Up to 54 Parser test cases

--- initial/src/test/ParserSuite.py
+++ initial/src/test/ParserSuite.py
@@ -57,14 +57,14 @@ class ParserSuite(unittest.TestCase):
         }'''
         self.assertTrue(TestParser.checkParser(input, success, 209))
 
-    def test_if_float(self):
+    def test_if_expint(self):
         input = '''int main() {
             if (a == 2)
                 return 2;
         }'''
         self.assertTrue(TestParser.checkParser(input, success, 210))
 
-    def test_if_float(self):
+    def test_if_expfloat(self):
         input = '''int main() {
             if (a != 2)
                 return 2.5;
@@ -96,3 +96,221 @@ class ParserSuite(unittest.TestCase):
             a = 2;
         }'''
         self.assertTrue(TestParser.checkParser(input, success, 216))
+
+    def test_local_global(self):
+        input = ''' float a;
+        int main() {
+            int b;
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 217))
+
+    def test_void(self):
+        input = '''void main() {
+            printf(1);
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 218))
+
+    def test_array(self):
+        input = '''int main() {
+            int arr[5];
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 219))
+
+    def test_wrong_array(self):
+        input = '''int main() {
+            int arr[];
+        }'''
+        self.assertTrue(TestParser.checkParser(input, '''Error on line 2 col 20: ]''', 220))
+
+    def test_array_void(self):
+        input = '''int main() {
+            void arr[5];
+        }'''
+        self.assertTrue(TestParser.checkParser(input, 'Error on line 2 col 12: void', 221))
+
+    def test_array_float(self):
+        input = '''int main() {
+            float arr[10];
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 222))
+
+    def test_array_bool(self):
+        input = '''void main() {
+            boolean arr[100];
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 223))
+
+    def test_pointer_func(self):
+        input = '''int[] bar() {
+            int c[3];
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 224))
+
+    def test_pointer_para(self):
+        input = '''int main(int argv[]){
+            return 0;
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 225))
+
+    def test_pointer(self):
+        input = '''int[] main(int argv[], int argc) {
+            return 0;
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 226))
+
+    def test_missed_para(self):
+        input = '''int main(int argv[], int ) {
+            return 0;
+        }'''
+        self.assertTrue(TestParser.checkParser(input, 'Error on line 1 col 25: )', 227))
+
+    def test_missed_outpara(self):
+        input = '''int main(int argv[){
+            print(1);
+        }'''
+        self.assertTrue(TestParser.checkParser(input, 'Error on line 1 col 18: )', 228))
+
+    def test_return_arr(self):
+        input = '''int main(int argc){
+            return argv[3];
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 229))
+
+    def test_missed_paratype(self):
+        input = '''int main(int argc, argv[]){
+            return 'OK';
+        }'''
+        self.assertTrue(TestParser.checkParser(input, 'Error on line 1 col 19: argv', 230))
+
+    def test_2d_array(self):
+        input = '''int main() {
+            int foo[2][3];
+        }'''
+        self.assertTrue(TestParser.checkParser(input, 'Error on line 2 col 22: [', 231))
+
+    def test_2d_assign(self):
+        input = '''int main() {
+            foo[5][5] = 1;
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 232))
+
+    def test_2d_func(self):
+        input = '''int main(){
+            return foo(2)[3];
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 233))
+
+    def test_2d_func_assign(self):
+        input = '''int main() {
+            foo(2)[3] = 1+1;
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 234))
+
+    def test_func_call(self):
+        input = '''void main () {
+            int pain; 
+            float train; 
+            try();
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 235))
+
+    def test_only_funccall(self):
+        input = '''int main(){
+            printf("OK");
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 236))
+
+    def test_break_stmt(self):
+        input = '''int main() {
+            break;
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 237))
+
+    def test_wrong_break(self):
+        input = '''void try() {
+            break 1;
+        }'''
+        self.assertTrue(TestParser.checkParser(input, 'Error on line 2 col 18: 1', 238))
+
+    def test_continue_stmt(self):
+        input = '''void main() {
+            if(true)
+                continue;
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 239))
+
+    def test_wrong_continue(self):
+        input = '''float main() {
+            continue "here";
+        }'''
+        self.assertTrue(TestParser.checkParser(input, '''Error on line 2 col 21: here''', 240))
+
+    def test_return_bool(self):
+        input = '''int main() { return a == b;}'''
+        self.assertTrue(TestParser.checkParser(input, success, 241))
+
+    def test_return_not(self):
+        input = '''int main() { return !a;}'''
+        self.assertTrue(TestParser.checkParser(input, success, 242))
+
+    def test_return_add(self):
+        input = '''int man() { return 1 + 2;}'''
+        self.assertTrue(TestParser.checkParser(input, success, 243))
+
+    def test_return_add_id(self):
+        input = '''int add() { return a + b;}'''
+        self.assertTrue(TestParser.checkParser(input, success, 244))
+
+    def test_return_or(self):
+        input = '''int or() {
+            return true || a;
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 245))
+
+    def test_return_assign(self):
+        input = '''void ass(int a, int b){
+            return a = b;
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 246))
+
+    def test_return_1d(self):
+        input = '''void arr(int a[]){ return a[3]; }'''
+        self.assertTrue(TestParser.checkParser(input, success, 247))
+
+    def test_return_arrInarr(self):
+        input = '''int main() { return a[b[3]]; }'''
+        self.assertTrue(TestParser.checkParser(input, success, 248))
+
+    def test_return_inverse(self):
+        input = '''int main() { return -id; }'''
+        self.assertTrue(TestParser.checkParser(input, success, 249))
+
+    def test_return_func(self):
+        input = '''int main() { return add(x, y); }'''
+        self.assertTrue(TestParser.checkParser(input, success, 250))
+
+    def test_return_enclosedBracket(self):
+        input = '''int main() { return (a == b || a != b);}'''
+        self.assertTrue(TestParser.checkParser(input, success, 251))
+
+    def test_if_else(self):
+        input = '''int main() { if (a < 2) return true; else return false;}'''
+        self.assertTrue(TestParser.checkParser(input, success, 252))
+
+    def test_if_block(self):
+        input = '''int main(){
+            if (a > 100){
+                a = 101;
+                b = 102;
+            }
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 253))
+
+    def test_elif(self):
+        input = '''int main(){
+            if (a > 10)
+                return a;
+            else if (a > 100)
+                return 10;
+        }'''
+        self.assertTrue(TestParser.checkParser(input, success, 254))
